3ef4969dc26a182448e28afebb19c79fab181141
lionel@lionel-pc2 <lionel.desorme@free.fr>
2020-05-08 15:16:17 +0200

    Start using config file

---
+++ ConfigHandler/BaseConfig.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace ConfigHandler
+{
+    public class BaseConfig
+    {
+        public static T Load<T>(string path)
+        {
+            return JsonConvert.DeserializeObject<T>(File.ReadAllText(path));
+        }
+
+        public void Save(string path)
+        {
+            File.WriteAllText(path, JsonConvert.SerializeObject(this, Formatting.Indented));
+        }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this, Formatting.Indented);
+        }
+    }
+}

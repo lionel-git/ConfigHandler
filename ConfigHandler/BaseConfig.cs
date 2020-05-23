d4b0bd1c3b6d3f1ce723553366d2e2fe62b29fdb
lionel@lionel-pc2 <lionel.desorme@free.fr>
2020-05-23 20:48:42 +0200

    Cleanup usings +jenkins batch

--- ConfigHandler/BaseConfig.cs
+++ ConfigHandler/BaseConfig.cs
@@ -1,11 +1,11 @@
-﻿using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using Newtonsoft.Json;
 
 namespace ConfigHandler
 {

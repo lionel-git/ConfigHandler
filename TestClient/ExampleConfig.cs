cfd6f90f4afaa108b51cc12b1ef42b7a4ddbdb2d
lionel@lionel-pc2 <lionel.desorme@free.fr>
2021-09-17 16:43:38 +0200

    Add example program for manual testing

---
+++ TestClient/ExampleConfig.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using ConfigHandler;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.ComponentModel;
+
+namespace TestClient
+{
+    // Just a dummy example
+    public class ExampleConfig : BaseConfig
+    {
+        [JsonConverter(typeof(StringEnumConverter))]
+        public enum Priority
+        {
+            Undefined,
+            Low,
+            Medium,
+            High
+        }
+
+        [EnvVar("EXAMPLE_BATCH_PRIORITY")]
+        [Option("Priority of batch")]
+        public Priority BatchPriority { get; set; }
+
+        [EnvVar("EXAMPLE_SERVER")]
+        public string Server { get; set; }
+
+        [Option("Test date", "yyyy/MM/dd")]
+        public DateTime TestDate { get; set; }
+
+        [Option("List of mail addresses")]
+        public List<string> MailRecipients { get; set; }
+
+        [Option("Level")]
+        [DefaultValue(true)]
+        public int Level { get; set; }
+
+        [Option("Some data")]
+        public List<string> Data { get; set; }
+
+        [Option("Test date (nullable)", "yyyy/MM/dd")]
+        public DateTime? TestDateNullable { get; set; }
+
+        public ExampleConfig()
+        {
+        }
+    }
+}
---
+++ TestClient/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ConfigHandler;
+
+namespace TestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var config = BaseConfig.LoadAll<ExampleConfig>("", args);
+                Console.WriteLine($"Config:\n{config}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
